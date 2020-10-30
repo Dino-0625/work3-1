35fe4e686c20a1a98549fc9d5d6266bfb9969cec
“Your <dinolee0625xd@gmail.com>
2020-10-30 21:38:15 +0800

    Initial Commit

---
+++ work3/work3App.swift
@@ -0,0 +1,17 @@
+//
+//  work3App.swift
+//  work3
+//
+//  Created by User03 on 2020/10/30.
+//
+
+import SwiftUI
+
+@main
+struct work3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
